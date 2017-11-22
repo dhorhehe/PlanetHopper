4626130ba06cb31cfb70f4328c65bbfbf6de116a
dhorhehe <thorg3@gmail.com>
2017-11-22 20:40:29 +0100

    menu klar

--- PlanetHopper/Assets/Scripts/SortHulBehaviour.cs
+++ PlanetHopper/Assets/Scripts/SortHulBehaviour.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SortHulBehaviour : MonoBehaviour {
-
+public class SortHulBehaviour : MonoBehaviour
+{
+    private float timer;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+     if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }   
 	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        teleport();
+    }
+
+    public void teleport()
+    {
+        timer = 2;
+    }
 }
